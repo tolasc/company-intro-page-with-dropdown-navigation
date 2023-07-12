cc01a02fc839fbe28c4697c29849108d0e61303d
Troy <tvinsixiengmay@gmail.com>
2023-07-12 03:06:10 -0700

    Add accordion menu to Features & Company

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var openMenu = document.getElementsByClassName('header-menu');
+var openMenus = document.getElementsByClassName('header-menu');
+var accordionMenus = document.getElementsByClassName('header-accordion');
+console.log(accordionMenus)
 var sideBar = document.getElementsByClassName('nav-content')[0];
-for(var i = 0; i < openMenu.length; i++) {
-    openMenu[i].addEventListener("click", function() {
+for(var i = 0; i < openMenus.length; i++) {
+    openMenus[i].addEventListener("click", function() {
         sideBar.classList.toggle('open');
+    });
+}
+for(var i = 0; i < accordionMenus.length; i++) {
+    accordionMenus[i].addEventListener("click", function() {
+        this.classList.toggle('open');
     });
 }
