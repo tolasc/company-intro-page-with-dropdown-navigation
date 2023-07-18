409f907e4334b6764cc8c58e370051075ab682d2
Troy <tvinsixiengmay@gmail.com>
2023-07-17 23:26:48 -0700

    Remove unneeded console print

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var openMenus = document.getElementsByClassName('header-menu');
 var accordionMenus = document.getElementsByClassName('header-accordion');
-console.log(accordionMenus)
 var sideBar = document.getElementsByClassName('nav-content')[0];
 for(var i = 0; i < openMenus.length; i++) {
     openMenus[i].addEventListener("click", function() {
